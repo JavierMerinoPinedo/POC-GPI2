ecd1962c5c201d22e09dac011252d45ec2848565
JavierMerinoPinedo <jmerinopinedo@gmail.com>
2025-12-27 00:02:17 +0100

    fix: Corregido test de interceptor y configuración OAuth2 para tests

--- ag-prueba-front/src/app/app.component.spec.ts
+++ ag-prueba-front/src/app/app.component.spec.ts
@@ -1,10 +1,23 @@
+//
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent // CAMBIO: De imports a declarations
+      ],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
   });
 
@@ -14,16 +27,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ag-prueba-front' title`, () => {
+  it(`should have as title 'ag-prueba-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ag-prueba-front');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ag-prueba-front');
   });
 });
--- ag-prueba-front/src/app/services/resource.service.spec.ts
+++ ag-prueba-front/src/app/services/resource.service.spec.ts
@@ -1,12 +1,19 @@
+//
 import { TestBed } from '@angular/core/testing';
-
 import { ResourceService } from './resource.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ResourceService', () => {
   let service: ResourceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(ResourceService);
   });
 
